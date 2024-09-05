a38236a91aeb9640e95e183808463059a268ccc3
fcmenkcv <fcmenkcv@gmail.com>
2024-09-05 23:59:56 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.pro.task06.task0606;
+
+/*
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    // напишите тут ваш код
+    public static void universalMethod(String s){
+
+    }
+
+    public static void universalMethod(String s1, String s2){
+
+    }
+
+    public static void universalMethod(String name, int age){
+
+    }
+
+    public static void universalMethod(int x){
+
+    }
+
+    public static void universalMethod(int x, int y){
+
+    }
+
+    public static void universalMethod(double d){
+
+    }
+
+    public static void universalMethod(double d, double b){
+
+    }
+
+    public static void universalMethod(String name, double weight){
+
+    }
+
+    public static void universalMethod(double tax, int perc){
+
+    }
+}
